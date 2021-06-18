7427eeece2c72853ba09b63b9903f3041f54569e
svyatoslav <valilossa@gmail.com>
2021-06-18 22:08:07 +0300

    check errors

--- src/com/battle/Main.java
+++ src/com/battle/Main.java
@@ -1,6 +1,8 @@
 package com.battle;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
@@ -10,33 +12,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var playerCards = IntStream.range(0,12).boxed().collect(toList());
+        var playerCards = IntStream.range(0, 12).boxed().collect(toList());
         var botCards = new ArrayList<>(playerCards);
 
-        int cardOne, cardTwo, i = 12, cardValFirst, cardValSec;
+        int cardOne = 0, cardTwo, cardValSec;
         int endSumFirstPl = 0, endSumSecPl = 0;
-        boolean isPlayerMove = true;
+        boolean isPlayerMove = true, errorInput;
 
         Scanner in = new Scanner(System.in);
 
         do {
-            if (isPlayerMove ) {
+            if (isPlayerMove) {
                 System.out.print("Первый игрок, введите номинал карты из оставшихся карт: ");
                 playerCards.forEach(a -> System.out.print(a + " "));
                 System.out.println();
 
-                cardOne = in.nextInt();
-                cardValFirst = playerCards.get(playerCards.indexOf(cardOne));
+                cardOne = getConsoleValue(true, playerCards, in);
 
                 cardTwo = 1 + (int) (Math.random() * (botCards.size() - 1));
                 cardValSec = botCards.get(cardTwo - 1);
                 System.out.println("Игрок 2 положил карту");
 
-                if (cardValFirst > cardValSec)
-                    endSumSecPl = endSumSecPl + (cardValFirst - cardValSec);
+                if (cardOne > cardValSec)
+                    endSumSecPl = endSumSecPl + (cardOne - cardValSec);
 
                 System.out.println("\nВскрываем карты:");
-                System.out.println("Первый игрок вытянул карту: " + cardValFirst);
+                System.out.println("Первый игрок вытянул карту: " + cardOne);
                 System.out.println("Второй игрок вытянул карту: " + cardValSec);
 
                 System.out.println("\n" + "-- Переход кода --" + "\n");
@@ -51,30 +52,53 @@ public class Main {
                 playerCards.forEach(a -> System.out.print(a + " "));
                 System.out.println();
 
-                cardOne = in.nextInt();
-                cardValFirst = playerCards.get(playerCards.indexOf(cardOne));
+                cardOne = getConsoleValue(true, playerCards, in);
 
-                if (cardValSec > cardValFirst)
-                    endSumFirstPl = endSumFirstPl + (cardValSec - cardValFirst);
+                if (cardValSec > cardOne)
+                    endSumFirstPl = endSumFirstPl + (cardValSec - cardOne);
 
                 System.out.println("\nВскрываем карты:");
                 System.out.println("Второй игрок вытянул карту: " + cardValSec);
-                System.out.println("Первый игрок вытянул карту: " + cardValFirst);
+                System.out.println("Первый игрок вытянул карту: " + cardOne);
 
                 System.out.println("\n" + "-- Переход кода --" + "\n");
 
                 isPlayerMove = true;
             }
 
-            playerCards.remove(playerCards.indexOf(cardOne));
+            playerCards.remove((Integer) cardOne);
             botCards.remove(cardTwo - 1);
 
-            i--;
-        } while (i != 0);
+        } while (playerCards.size() != 0);
 
         checkResult(endSumFirstPl, endSumSecPl);
 
         in.close();
+    }
+
+    private static int getConsoleValue(boolean errorInput, List<Integer> playerCards, Scanner in) {
+
+        int inputValue = 0;
+
+        while (errorInput) {
+            try {
+                if (in.hasNextInt()) {
+                    inputValue = in.nextInt();
+                    if (inputValue < 0 || !playerCards.contains(inputValue)) {
+                        System.out.println("Введите число из списка оставшихся!");
+                        continue;
+                    }
+                } else {
+                    in.next();
+                    System.out.println("Введите число из списка оставшихся!");
+                    continue;
+                }
+                errorInput = false;
+            } catch (InputMismatchException ime) {
+                System.out.println("Введите число из списка оставшихся!");
+            }
+        }
+        return inputValue;
     }
 
     private static void checkResult(int endSumFirstPl, int endSumSecPl) {
