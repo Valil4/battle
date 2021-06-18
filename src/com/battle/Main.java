6af5008c53e7edb047167f47410c4e22530884ce
svyatoslav <valilossa@gmail.com>
2021-06-18 19:58:35 +0300

    rereading task

--- src/com/battle/Main.java
+++ src/com/battle/Main.java
@@ -2,53 +2,32 @@ package com.battle;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> arrFirstPlCards = new ArrayList<>();
-        arrFirstPlCards.add(0, 5);
-        arrFirstPlCards.add(1, 0);
-        arrFirstPlCards.add(2, 11);
-        arrFirstPlCards.add(3, 4);
-        arrFirstPlCards.add(4, 7);
-        arrFirstPlCards.add(5, 9);
-        arrFirstPlCards.add(6, 2);
-        arrFirstPlCards.add(7, 1);
-        arrFirstPlCards.add(8, 6);
-        arrFirstPlCards.add(9, 3);
-        arrFirstPlCards.add(10, 8);
-        arrFirstPlCards.add(11, 10);
+        var playerCards = IntStream.range(0,12).boxed().collect(toList());
+        var botCards = new ArrayList<>(playerCards);
 
-        ArrayList<Integer> arrSecondPlCards = new ArrayList<>();
-        arrSecondPlCards.add(0, 9);
-        arrSecondPlCards.add(1, 2);
-        arrSecondPlCards.add(2, 0);
-        arrSecondPlCards.add(3, 11);
-        arrSecondPlCards.add(4, 4);
-        arrSecondPlCards.add(5, 5);
-        arrSecondPlCards.add(6, 10);
-        arrSecondPlCards.add(7, 7);
-        arrSecondPlCards.add(8, 1);
-        arrSecondPlCards.add(9, 6);
-        arrSecondPlCards.add(10, 3);
-        arrSecondPlCards.add(11, 8);
-
-        int cardOne, cardTwo, i = 12, currentPlayer = 1, cardValFirst, cardValSec;
+        int cardOne, cardTwo, i = 12, cardValFirst, cardValSec;
         int endSumFirstPl = 0, endSumSecPl = 0;
+        boolean isPlayerMove = true;
 
         Scanner in = new Scanner(System.in);
 
         do {
-            if (currentPlayer == 1) {
-                System.out.println("Первый игрок, вытяните карту");
+            if (isPlayerMove ) {
+                System.out.println("Первый игрок, введите номинал карты из оставшихся карт: ");
                 cardOne = in.nextInt();
-                cardValFirst = arrSecondPlCards.get(cardOne - 1);
+                cardValFirst = playerCards.get(playerCards.indexOf(cardOne));
 
-                cardTwo = 1 + (int) (Math.random() * (arrFirstPlCards.size() - 1));
-                cardValSec = arrFirstPlCards.get(cardTwo - 1);
-                System.out.println("Игрок 2 вытянул карту");
+                cardTwo = 1 + (int) (Math.random() * (botCards.size() - 1));
+                cardValSec = botCards.get(cardTwo - 1);
+                System.out.println("Игрок 2 положил карту");
 
                 if (cardValFirst > cardValSec)
                     endSumSecPl = endSumSecPl + (cardValFirst - cardValSec);
@@ -59,15 +38,15 @@ public class Main {
 
                 System.out.println("\n" + "-- Переход кода --" + "\n");
 
-                currentPlayer = 2;
+                isPlayerMove = false;
             } else {
-                cardTwo = 1 + (int) (Math.random() * (arrFirstPlCards.size() - 1));
-                cardValSec = arrFirstPlCards.get(cardTwo - 1);
-                System.out.println("Игрок 2 вытянул карту");
+                cardTwo = 1 + (int) (Math.random() * (botCards.size() - 1));
+                cardValSec = botCards.get(cardTwo - 1);
+                System.out.println("Игрок 2 положил карту");
 
-                System.out.println("Первый игрок, вытяните карту");
+                System.out.println("Первый игрок, введите номинал карты из оставшихся карт: ");
                 cardOne = in.nextInt();
-                cardValFirst = arrSecondPlCards.get(cardOne - 1);
+                cardValFirst = playerCards.get(playerCards.indexOf(cardOne));
 
                 if (cardValSec > cardValFirst)
                     endSumFirstPl = endSumFirstPl + (cardValSec - cardValFirst);
@@ -78,12 +57,11 @@ public class Main {
 
                 System.out.println("\n" + "-- Переход кода --" + "\n");
 
-                currentPlayer = 1;
+                isPlayerMove = true;
             }
 
-            
-            arrFirstPlCards.remove(cardTwo - 1);
-            arrSecondPlCards.remove(cardOne - 1);
+            playerCards.remove(cardTwo - 1);
+            botCards.remove(cardOne - 1);
 
             i--;
         } while (i != 0);
